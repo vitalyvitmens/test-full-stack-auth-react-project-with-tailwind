f1b393d745bbccb15f1d2638c03563a2fa6f488d
vitalyvitmens <otm_@tut.by>
2023-11-15 22:46:45 +0300

    add author in WalkthroughsPage

--- client/src/pages/WalkthroughsPage.jsx
+++ client/src/pages/WalkthroughsPage.jsx
@@ -6,6 +6,8 @@ import {
 	selectWalkthroughs,
 	selectUserFirstName,
 	selectUserLastName,
+	selectUsers,
+	loadUsersAsync,
 } from '../redux'
 import Moment from 'react-moment'
 import { useNavigate } from 'react-router-dom'
@@ -18,13 +20,14 @@ export const WalkthroughsPage = ({ score, numQuestions, onRestart }) => {
 	const firstName = useSelector(selectUserFirstName)
 	const lastName = useSelector(selectUserLastName)
 	const walkthroughs = useSelector(selectWalkthroughs)
+	const user = useSelector(selectUsers)
 
 	useLayoutEffect(() => {
 		setIsLoading(true)
 
-		dispatch(loadWalkthroughsAsync()).then(() => {
-			setIsLoading(false)
-		})
+		dispatch(loadWalkthroughsAsync())
+		dispatch(loadUsersAsync())
+		setIsLoading(false)
 	}, [dispatch, numQuestions, score])
 
 	if (!walkthroughs.length) {
@@ -33,6 +36,10 @@ export const WalkthroughsPage = ({ score, numQuestions, onRestart }) => {
 
 	const onTitleChange = ({ target }) => {
 		setTitleValue(target.value)
+	}
+
+	if (!walkthroughs.length | !user.data) {
+		return <Loader />
 	}
 
 	return (
@@ -51,7 +58,10 @@ export const WalkthroughsPage = ({ score, numQuestions, onRestart }) => {
 				<div>
 					Автор теста: {lastName} {firstName}
 				</div>
-				<div>Дата создания: 05.02.2003</div>
+				<div>
+					Дата создания:{' '}
+					<Moment date={Date.now()} format="DD.MM.YYYYг. HH:mm" />
+				</div>
 			</div>
 			<div className="py-5">
 				<Button
@@ -66,46 +76,50 @@ export const WalkthroughsPage = ({ score, numQuestions, onRestart }) => {
 			<h2 className="text-3xl text-center text-blue-800">
 				История прохождений
 			</h2>
-			{!isLoading &&
-				walkthroughs.map(
-					({
-						_id,
-						title,
-						author,
-						numQuestions,
-						numCorrectAnswers,
-						createdAt,
-					}) => (
-						<ul className="w-full flex flex-col items-center" key={_id}>
-							<li className="flex flex-row p-1">
-								<div className="flex flex-row border w-[700px]  border-black rounded-md px-4 py-2 justify-between">
-									<div className="flex flex-col px-4 py-2 text-xl">
-										{title}
-										<div className="text-base">Автор теста: {author}</div>
-										<Moment
-											className="text-xs"
-											date={createdAt}
-											format="DD.MM.YYYYг. HH:mm"
-										/>
-									</div>
-									{numCorrectAnswers / numQuestions <= 0.3 && (
-										<div className="bg-bad bg-cover h-[80px] w-[80px] bg-center"></div>
-									)}
-									{numCorrectAnswers / numQuestions > 0.3 &&
-										numCorrectAnswers / numQuestions < 0.7 && (
-											<div className="bg-norm bg-cover h-[80px] w-[80px] bg-center"></div>
-										)}
-									{numCorrectAnswers / numQuestions >= 0.7 && (
-										<div className="bg-good bg-cover h-[80px] w-[80px] bg-center"></div>
-									)}
-									<div className="flex flex-col justify-center text-xl">
-										Верно: {numCorrectAnswers} из {numQuestions}
-									</div>
+			{walkthroughs.map(
+				({
+					_id,
+					title,
+					author,
+					numQuestions,
+					numCorrectAnswers,
+					createdAt,
+				}) => (
+					<ul className="w-full flex flex-col items-center" key={_id}>
+						<li className="flex flex-row p-1">
+							<div className="flex flex-row border w-[700px]  border-black rounded-md px-4 py-2 justify-between">
+								<div className="flex flex-col px-4 py-2 text-xl">
+									{title}
+									<div className="pr-1 text-lg">Автор теста: </div>
+									{user.data.map(({ id, lastName, firstName }) => (
+										<div key={id} className="flex flex-row text-lg">
+											{author === id && lastName} {author === id && firstName}
+										</div>
+									))}
+									<Moment
+										className="text-xs"
+										date={createdAt}
+										format="DD.MM.YYYYг. HH:mm"
+									/>
 								</div>
-							</li>
-						</ul>
-					)
-				)}
+								{numCorrectAnswers / numQuestions <= 0.3 && (
+									<div className="bg-bad bg-cover h-[120px] w-[120px] bg-center"></div>
+								)}
+								{numCorrectAnswers / numQuestions > 0.3 &&
+									numCorrectAnswers / numQuestions < 0.7 && (
+										<div className="bg-norm bg-cover h-[120px] w-[120px] bg-center"></div>
+									)}
+								{numCorrectAnswers / numQuestions >= 0.7 && (
+									<div className="bg-good bg-cover h-[120px] w-[120px] bg-center"></div>
+								)}
+								<div className="flex flex-col justify-center text-xl">
+									Верно: {numCorrectAnswers} из {numQuestions}
+								</div>
+							</div>
+						</li>
+					</ul>
+				)
+			)}
 		</div>
 	)
 }

9aa566e1ff1ce97301dec70b3d4a161922b03781
vitalyvitmens <otm_@tut.by>
2023-11-15 13:19:37 +0300

    fix styles in MainPage

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -4,6 +4,7 @@ import { Button, Loader } from '../components'
 import { useDispatch, useSelector } from 'react-redux'
 import Moment from 'react-moment'
 import { loadWalkthroughsAsync, selectWalkthroughs } from '../redux'
+import { WalkthroughsPage } from './WalkthroughsPage'
 
 export const MainPage = () => {
 	const navigate = useNavigate()
@@ -27,42 +28,39 @@ export const MainPage = () => {
 			<h2 className="mt-2 mb-4 text-3xl text-center text-blue-800">
 				История прохождений
 			</h2>
-			<div className="p-10 flex flex-col justify-between border border-amber-950 rounded-md">
-				<div className="w-full flex flex-col items-center">
-					{walkthroughs.map(
-						({
-							_id,
-							title,
-							author,
-							numQuestions,
-							numCorrectAnswers,
-							createdAt,
-						}) => (
-							<ul className="w-[70%]" key={_id}>
-								<li className="my-2 py-2 px-4 flex justify-between border border-amber-950 rounded-md">
-									<div className="text-blue-800">
-										<Moment date={createdAt} format="DD.MM.YYYYг. HH:mm" />
-									</div>
-									<div className="h-9 w-9">
-										{numCorrectAnswers / numQuestions <= 0.3 && (
-											<div className="bg-bad bg-cover h-[40px] w-[40px] bg-center"></div>
-										)}
-										{numCorrectAnswers / numQuestions > 0.3 &&
-											numCorrectAnswers / numQuestions < 0.7 && (
-												<div className="bg-norm bg-cover h-[40px] w-[40px] bg-center"></div>
-											)}
-										{numCorrectAnswers / numQuestions >= 0.7 && (
-											<div className="bg-good bg-cover h-[40px] w-[40px] bg-center"></div>
-										)}
-									</div>
-									<p className="text-blue-800">
-										Верно {numCorrectAnswers} из {numQuestions}
-									</p>
-								</li>
-							</ul>
-						)
-					)}
-				</div>
+			<div className="grid grid-rows-2 grid-flow-col gap-3">
+				{walkthroughs.map(
+					({
+						_id,
+						title,
+						author,
+						numQuestions,
+						numCorrectAnswers,
+						createdAt,
+					}) => (
+						<ul className="" key={_id}>
+							<li className="flex flex-col p-2 justify-between border border-amber-950 rounded-md text-sm">
+								<div className=" text-xl text-black">Название теста:</div>
+								<div className=" text-xl text-black">{title}</div>
+								<div className="p-4 text-blue-800">{_id}</div>
+								<div>Количество вопросов: {numQuestions}</div>
+								<div>Автор теста: {author}</div>
+								<div className="flex flex-row text-xs pt-2 justify-between">
+									Дата создания:
+									<Moment date={createdAt} format="DD.MM.YYYYг. HH:mm" />
+								</div>
+								<div className="flex justify-center pt-5">
+									<Button
+										title="Открыть"
+										bgColor="bg-green-800"
+										fontSize="text-xl"
+										onClick={() => navigate('/walkthroughs')}
+									/>
+								</div>
+							</li>
+						</ul>
+					)
+				)}
 			</div>
 		</>
 	)
